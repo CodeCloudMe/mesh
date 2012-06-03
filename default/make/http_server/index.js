2390de9f80e63fff4106fee06e7a65df9932d3ca
Craig Condon <craig@spiceapps.com>
2012-06-02 19:14:49 -0500

    added http server

---
+++ default/make/http_server/index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+exports.params = {
+	'directory': true
+};
+
+exports.run = function(target, next) {
+	var server = express.createServer();
+	server.use(express.static(target.directory));	
+	server.listen(target.port || 8080);
+}
+
+exports.taskMessage = function(target) {
+	return "run http server dir=" + target.directory + " port=" + target.port; 
+}
